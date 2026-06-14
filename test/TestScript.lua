ba0853c70d9a45f3c91697c50c2cd63ccf515b8d
YumYum16 <raphael032009@gmail.com>
2026-06-14 15:32:42 +0200

    test: ajout d'un script Lua de test Roblox

---
+++ test/TestScript.lua
@@ -0,0 +1,9 @@
+-- Script de test créé par Claude
+-- Tu peux copier ce code dans Roblox Studio
+
+local function direBonjour(nom)
+	print("Bonjour " .. nom .. " !")
+end
+
+direBonjour("Joueur")
+print("Le script de test fonctionne !")
